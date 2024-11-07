1f655f5ebf5cf0044db274d95aa1b04f85af04f4
sovaKrya <tyan_34ee@mail.ru>
2024-11-07 17:50:41 +0300

    aoperation watch added

--- src/components/TheSettings.vue
+++ src/components/TheSettings.vue
@@ -1,8 +1,72 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const durationGame = ref(1)
 const complexityGame = ref(1)
+
+const operations = ref({
+  addition: false,
+  subtraction: false,
+  multiplication: false,
+  division: false,
+  exponentiation: false,
+})
+
+const signs = ref<Array<'+' | '-' | '*' | '/' | '^'>>([])
+
+watch(
+  operations.value,
+  (newOp) => {
+    if (newOp.addition) {
+      signs.value.push('+')
+      console.log(signs.value)
+      return
+    } else {
+      const idx = signs.value.findIndex((operation) => operation === '+')
+      signs.value.splice(idx, 1)
+      console.log(signs.value)
+    }
+
+    if (newOp.division) {
+      signs.value.push('/')
+      console.log(signs.value)
+      return
+    } else {
+      const idx = signs.value.findIndex((operation) => operation === '/')
+      signs.value.splice(idx, 1)
+      console.log(signs.value)
+    }
+
+    if (newOp.subtraction) {
+      signs.value.push('-')
+      console.log(signs.value)
+      return
+    } else {
+      const idx = signs.value.findIndex((operation) => operation === '-')
+      signs.value.splice(idx, 1)
+      console.log(signs.value)
+    }
+    if (newOp.multiplication) {
+      signs.value.push('*')
+      console.log(signs.value)
+      return
+    } else {
+      const idx = signs.value.findIndex((operation) => operation === '*')
+      signs.value.splice(idx, 1)
+      console.log(signs.value)
+    }
+    if (newOp.exponentiation) {
+      signs.value.push('^')
+      console.log(signs.value)
+      return
+    } else {
+      const idx = signs.value.findIndex((operation) => operation === '^')
+      signs.value.splice(idx, 1)
+      console.log(signs.value)
+    }
+  },
+  { deep: true },
+)
 </script>
 
 <template>
@@ -10,9 +74,13 @@ const complexityGame = ref(1)
     <div class="duration-box">
       <h2>Настройки</h2>
       <div>
+        <div class="duraion-input-title-box">
+          <span>1</span>
+          <span>15</span>
+        </div>
         <input type="range" min="1" max="15" v-model="durationGame" class="duraion-input" />
         <div class="duraion-text">
-          <span>Длительность {{ durationGame }} </span>
+          <span style="width: 122px">Длительность {{ durationGame }} </span>
           <span v-if="durationGame < 2">минута</span>
           <span v-else-if="durationGame < 5 && durationGame > 1">минуты</span>
           <span v-else-if="durationGame >= 5">минут</span>
@@ -20,6 +88,10 @@ const complexityGame = ref(1)
       </div>
 
       <div class="inputs-box">
+        <div class="duraion-input-title-box">
+          <span>1</span>
+          <span>15</span>
+        </div>
         <input type="range" min="1" max="15" v-model="complexityGame" class="duraion-input" />
         <span>Сложность {{ complexityGame }}</span>
       </div>
@@ -27,23 +99,23 @@ const complexityGame = ref(1)
 
     <div class="settings-signs-wrapper">
       <div class="settings-signs-box">
-        <input type="checkbox" class="sign-checkbox" />
+        <input type="checkbox" class="sign-checkbox" v-model="operations.addition" />
         <span>Суммирование</span>
       </div>
       <div class="settings-signs-box">
-        <input type="checkbox" class="sign-checkbox" />
+        <input type="checkbox" class="sign-checkbox" v-model="operations.subtraction" />
         <span>Разность</span>
       </div>
       <div class="settings-signs-box">
-        <input type="checkbox" class="sign-checkbox" />
+        <input type="checkbox" class="sign-checkbox" v-model="operations.multiplication" />
         <span>Умножение</span>
       </div>
       <div class="settings-signs-box">
-        <input type="checkbox" class="sign-checkbox" />
+        <input type="checkbox" class="sign-checkbox" v-model="operations.division" />
         <span>Деление</span>
       </div>
       <div class="settings-signs-box">
-        <input type="checkbox" class="sign-checkbox" />
+        <input type="checkbox" class="sign-checkbox" v-model="operations.exponentiation" />
         <span>Возведение в степень</span>
       </div>
     </div>
@@ -63,6 +135,12 @@ const complexityGame = ref(1)
   display: flex;
   flex-direction: column;
   gap: 16px;
+}
+
+.duraion-input-title-box {
+  display: flex;
+  justify-content: space-between;
+  width: 300px;
 }
 
 .duraion-input {
